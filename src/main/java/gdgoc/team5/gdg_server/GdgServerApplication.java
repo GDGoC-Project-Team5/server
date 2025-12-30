4e242fedee184e99de951b33b00f5581f44a5e8d
ChooJG <cnwprms@naver.com>
2025-12-30 19:07:25 +0900

    first commit!!

---
+++ src/main/java/gdgoc/team5/gdg_server/GdgServerApplication.java
@@ -0,0 +1,13 @@
+package gdgoc.team5.gdg_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdgServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdgoc/team5/gdg_server/GdgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package gdgoc.team5.gdg_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
